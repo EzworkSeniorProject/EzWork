f2f390a836a3d5245b2dd07a58f2aa344666e720
Your Name <leobarrientos02@gmail.com>
2021-10-21 15:38:15 -0400

    edit location css, html, and javascript added

--- PostAJob/reviewJob.js
+++ PostAJob/reviewJob.js
@@ -2187,3 +2187,143 @@ applyScopeEdit.addEventListener("click", () => {
       .concat(currentScopeExperience)
       .concat(" experience."));
 });
+
+//Review Location
+const usOnlyCard = document.getElementById("usOnly");
+const worldwideCard = document.getElementById("worldwide");
+
+//Buttons for cards
+const worldwideBtn = document.getElementById("worlwide-button");
+const usOnlyBtn = document.getElementById("usOnly-button");
+
+// Us locations And worldwide locations
+const usOnlyLocations = document.querySelector(".usOnlyCard");
+const worldwideLocations = document.querySelector(".worldwideCard");
+
+const allLocations = [];
+worldwideCard.addEventListener("click", () => {
+  worldwideCard.style.border = "1.25px solid #054e97";
+  worldwideBtn.style.color = "#054e97";
+  worldwideBtn.style.border = "1.25px solid #054e97";
+
+  usOnlyCard.style.border = "1.25px solid lightgrey";
+  usOnlyBtn.style.color = "transparent";
+  usOnlyBtn.style.border = "1.25px solid lightgrey";
+
+  worldwideLocations.style.display = "inline-block";
+  usOnlyLocations.style.display = "none";
+
+  //Ensure the us data is clear
+  //allLocations.splice(0, allLocations.length);
+  selectedLocationsContainer.style.display = "none";
+  selectedLocationsCard.style.display = "none";
+});
+usOnlyCard.addEventListener("click", () => {
+  usOnlyCard.style.border = "1.25px solid #054e97";
+  usOnlyBtn.style.color = "#054e97";
+  usOnlyBtn.style.border = "1.25px solid #054e97";
+
+  worldwideCard.style.border = "1.25px solid lightgrey";
+  worldwideBtn.style.color = "transparent";
+  worldwideBtn.style.border = "1.25px solid lightgrey";
+
+  usOnlyLocations.style.display = "inline-block";
+  worldwideLocations.style.display = "none";
+
+  //Ensure the worldwide data is clear
+  //allLocations.splice(0, allLocations.length);
+  selectedLocationsContainer2.style.display = "none";
+  selectedLocationsCard2.style.display = "none";
+});
+
+const tmz = document.getElementById("TimeZones&StatesID");
+const sr = document.getElementById("States&RegionsID");
+
+const addBtn1 = document.getElementById("add1");
+const addBtn2 = document.getElementById("add2");
+
+const selectedLocationsCard = document.querySelector(
+  ".selectedLocationsUSCard"
+);
+const selectedLocationsContainer = document.querySelector(
+  ".selectedLocationsUS"
+);
+
+const selectedLocationsCard2 = document.querySelector(
+  ".selectedLocationsWorldwideCard"
+);
+const selectedLocationsContainer2 = document.querySelector(
+  ".selectedLocationsWorldwide"
+);
+
+const emptyError = document.getElementById("emptyError");
+
+addBtn1.addEventListener("click", () => {
+  if (tmz.value === "") {
+    emptyError.innerText = "No input.";
+  } else if (allLocations.includes(tmz.value) === true) {
+    // Ensure duplicates are not entered
+    emptyError.innerText = "Already added!";
+    //console.log("leo");
+  } else {
+    selectedLocationsCard2.style.display = "none";
+    selectedLocationsContainer2.style.display = "none";
+
+    selectedLocationsContainer.style.display = "inline-block";
+    //console.log(e.value);
+    var locationPill = document.createElement("label");
+    locationPill.innerHTML = tmz.value;
+
+    allLocations.push(tmz.value);
+
+    selectedLocationsCard.style.display = "inline-block";
+    selectedLocationsContainer.style.display = "inline-block";
+    selectedLocationsContainer.appendChild(locationPill);
+
+    //console.log(allLocations);
+
+    if (allLocations.length > 0) {
+      locationPill.addEventListener("click", () => {
+        selectedLocationsContainer.removeChild(locationPill);
+        allLocations.pop(locationPill.innerHTML);
+        //console.log(allLocations);
+      });
+    } else if ((allLocations.length = 0)) {
+    }
+  }
+});
+
+addBtn2.addEventListener("click", () => {
+  if (sr.value === "") {
+    emptyError.innerText = "No input.";
+  } else if (allLocations.includes(sr.value) === true) {
+    // Ensure duplicates are not entered
+    emptyError.innerText = "Already added!";
+    //console.log("leo");
+  } else {
+    selectedLocationsCard.style.display = "none";
+    selectedLocationsContainer.style.display = "none";
+    //console.log(a.value);
+    var locationPill = document.createElement("label");
+    locationPill.innerHTML = sr.value;
+
+    allLocations.push(sr.value);
+
+    selectedLocationsCard2.style.display = "inline-block";
+    selectedLocationsContainer2.style.display = "inline-block";
+
+    selectedLocationsContainer2.appendChild(locationPill);
+    //console.log(allLocations);
+
+    if (allLocations.length > 0) {
+      locationPill.addEventListener("click", () => {
+        selectedLocationsContainer2.removeChild(locationPill);
+        allLocations.pop(locationPill.innerHTML);
+        //console.log(allLocations);
+      });
+    } else if ((allLocations.length = 0)) {
+    }
+  }
+});
+
+const EmptyArrayError = document.getElementById("emptyArray");
